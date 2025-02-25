5093ffc86a2d3b0e2b7a1829b0e7c49537e13d9e
Miguel Serra <miguel.serra.estrada@gmail.com>
2025-02-24 19:54:59 -0500

    feature(LLM): Integrated bot with Gemini LLM via bot commands

--- src/entry.py
+++ src/entry.py
@@ -1,21 +1,45 @@
 import os
 import discord
+from discord.ext import commands
+from gemini import Gemini
 
 from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-
-
 intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!",intents=intents)
+gemini = Gemini()
 
-@client.event
-async def on_ready():
-  print(f"We have logged in as {client.user}")
+chunkLen = 1985
 
-client.run(DISCORD_TOKEN)
+@bot.command(name="ask",help="Talk to the bot")
+async def query(ctx, *, arg):
+  messageObj = ctx.message
+  await messageObj.add_reaction('\N{THUMBS UP SIGN}')  
+  response = gemini.query_gemini(arg)
+  await ctx.send(response)
+  
+@bot.command(name="chat", help="Chat with context")
+async def query_with_context(ctx, *, arg):
+  messageObj = ctx.message
+  await messageObj.add_reaction('\N{THUMBS UP SIGN}')
+  response = gemini.chat_with_context(arg)
+
+  if len(response) >= chunkLen:
+    response_chunks = [response[i: i + chunkLen] for i in range(0, len(response), chunkLen)]
+    for i, chunk in enumerate(response_chunks):
+      if i != len(response_chunks) - 1:
+        await ctx.send(f"(Part {i+1}): {chunk}...\n")
+      else:
+        await ctx.send(f"(Part {i+1}): {chunk}")
+  else: 
+    await ctx.send(response)
+
+bot.run(DISCORD_TOKEN)
 
 
 
---
+++ src/gemini.py
@@ -0,0 +1,25 @@
+from google import genai
+from google.genai import types
+import os
+from dotenv import load_dotenv
+from PIL import Image
+
+class Gemini():
+  
+  def __init__(self):
+    load_dotenv()
+    GEMINI_KEY = os.getenv("GEMINI_KEY")
+    self.client =  genai.Client(api_key=GEMINI_KEY)
+    self.chat = self.client.chats.create(model="gemini-2.0-flash", config=types.GenerateContentConfig(
+      temperature=1.5,
+    ))
+
+  def query_gemini(self,input): 
+    response = self.client.models.generate_content(
+      model="gemini-2.0-flash", contents=input
+    )
+    return response.text
+
+  def chat_with_context(self,input):
+    response = self.chat.send_message(input)
+    return response.text
